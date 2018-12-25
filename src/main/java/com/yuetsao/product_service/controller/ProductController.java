71e9b6da4075cafe661d98bd85c824846a69722d
caoyue <yuetsao@foxmail.com>
2018-12-25 21:04:37 +0800

    不同的port调用

--- src/main/java/com/yuetsao/product_service/controller/ProductController.java
+++ src/main/java/com/yuetsao/product_service/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.yuetsao.product_service.controller;
 
+import com.yuetsao.product_service.domain.Product;
 import com.yuetsao.product_service.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/product")
 public class ProductController {
+
+    @Value("${server.port}")
+    private String port;
 
     @Autowired
     private ProductService productService;
@@ -20,6 +26,10 @@ public class ProductController {
 
     @RequestMapping("findById")
     public Object findById(@RequestParam("id") int id) {
-        return productService.findById(id);
+        Product product = productService.findById(id);
+        Product result = new Product();
+        BeanUtils.copyProperties(product, result);
+        result.setName(product.getName()+"data from" + port);
+        return result;
     }
 }
