5146da43187e6797d5fd4f7bbb17b5798c00484d
caoyue <yuetsao@foxmail.com>
2018-12-28 15:34:03 +0800

    睡眠模拟

--- src/main/java/com/yuetsao/product_service/ProductServiceApplication.java
+++ src/main/java/com/yuetsao/product_service/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.yuetsao.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
