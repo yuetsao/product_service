38c1b0250f6c1c155d8ce41145efbe891ad4aa74
caoyue <yuetsao@foxmail.com>
2019-01-09 11:44:14 +0800

    通过actuator/bus-refresh + @RefreshScope手动刷新配置

--- src/main/java/com/yuetsao/product_service/controller/ProductController.java
+++ src/main/java/com/yuetsao/product_service/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.yuetsao.product_service.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/v1/product")
+@RefreshScope
 public class ProductController {
 
     @Value("${server.port}")
